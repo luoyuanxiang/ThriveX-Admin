c561e89178f995d19656fdf536bcbb1b65c30a75
神秘人 <3311118881@qq.com>
2025-06-13 19:06:08 +0800

    优化DefaultLayout组件，移除多余的注释，提高代码可读性

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -7,29 +7,19 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
 
   return (
     <div className="dark:bg-[#1A222C] dark:text-bodydark">
-      {/* <!-- ===== Page Wrapper Start ===== --> */}
       <div className="flex h-screen overflow-hidden">
-        {/* <!-- ===== Sidebar Start ===== --> */}
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* <!-- ===== Sidebar End ===== --> */}
 
-        {/* <!-- ===== Content Area Start ===== --> */}
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          {/* <!-- ===== Header Start ===== --> */}
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          {/* <!-- ===== Header End ===== --> */}
 
-          {/* <!-- ===== Main Content Start ===== --> */}
           <main>
             <div className="mx-auto p-4">
               {children}
             </div>
           </main>
-          {/* <!-- ===== Main Content End ===== --> */}
         </div>
-        {/* <!-- ===== Content Area End ===== --> */}
       </div>
-      {/* <!-- ===== Page Wrapper End ===== --> */}
     </div>
   );
 };
