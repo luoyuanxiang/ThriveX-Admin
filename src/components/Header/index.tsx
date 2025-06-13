a0f57b4802ec5df3e084bbfe8528f55ee470b92b
神秘人 <3311118881@qq.com>
2025-06-13 20:52:15 +0800

    调整DropdownUser组件头像大小，优化Header组件的内边距，以提升整体视觉效果

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ const Header = (props: {
 }) => {
   return (
     <header className="sticky top-0 z-999 flex w-full bg-[rgba(255,255,255,0.5)] drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-      <div className="flex flex-grow items-center justify-between px-4 py-2 shadow-2 md:px-6 2xl:px-11">
+      <div className="flex flex-grow items-center justify-between px-4 py-3 shadow-2 md:px-6 2xl:px-11">
         <div className='flex items-center'>
           <div className="flex items-center gap-4 lg:hidden">
             <button
