8ae8c60ea061d007b1889b09fa3bc811a27373c9
神秘人 <3311118881@qq.com>
2025-06-16 20:45:35 +0800

    删除DockerfileDev、安装文档和手势运动文档，清理不必要的文件以简化项目结构。

--- src/pages/Web/index.tsx
+++ src/pages/Web/index.tsx
@@ -121,7 +121,7 @@ export default () => {
         return !value || /^(https?:\/\/)/.test(value) ? Promise.resolve() : Promise.reject(new Error('请输入有效的链接'));
     };
 
-    const submit = async () => {
+    const onSubmit = async () => {
         try {
             setBtnLoading(true)
 
@@ -229,7 +229,7 @@ export default () => {
 
                     <Spin spinning={editLoading}>
                         <div className='w-full md:w-[500px] mx-auto'>
-                            <Form form={form} layout="vertical" size='large' initialValues={link} onFinish={submit}>
+                            <Form form={form} layout="vertical" size='large' initialValues={link} onFinish={onSubmit}>
                                 <Form.Item label="网站标题" name="title" rules={[{ required: true, message: '网站标题不能为空' }]}>
                                     <Input placeholder="Thrive" />
                                 </Form.Item>
