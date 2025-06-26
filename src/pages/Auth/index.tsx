d9f3b1b3f5464aa2ce5acaf71511fb6385b899e6
神秘人 <3311118881@qq.com>
2025-06-26 14:16:19 +0800

    实现GitHub OAuth登录功能，处理code参数并在成功登录后保存用户信息和权限，同时添加登录成功和失败的通知提示。

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,3 +1,49 @@
+import { useEffect } from 'react';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { authGitHubLoginAPI } from '@/api/Auth'
+import { getRolePermissionListAPI } from '@/api/Role';
+import { useUserStore } from '@/stores';
+import { notification } from 'antd';
+
 export default () => {
-  return <h1>Hello</h1>
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate()
+  const store = useUserStore();
+
+  // 获取code参数
+  const code = searchParams.get('code');
+
+  const authGitHubLogin = async (code: string) => {
+    try {
+      const { data } = await authGitHubLoginAPI(code)
+      const { data: permission } = await getRolePermissionListAPI(data.role.id as number);
+
+      // 将用户信息和token保存起来
+      store.setToken(data.token);
+      store.setUser(data.user);
+      store.setRole(data.role)
+      store.setPermission(permission)
+
+      notification.success({
+        message: '🎉 登录成功',
+        description: `Hello ${data.user.name} 欢迎回来`,
+      });
+
+      navigate('/')
+    } catch (error) {
+      // 登录失败，重定向到登录页
+      navigate('/login')
+      // http://localhost:5173/auth?code=8ee96443633cca3aaded
+    }
+  }
+
+  useEffect(() => {
+    if (code) {
+      console.log('获取到的code值:', code);
+      // 在这里处理code，比如发送到后端进行OAuth认证
+      authGitHubLogin(code)
+    }
+  }, [code]);
+
+  return null
 }
