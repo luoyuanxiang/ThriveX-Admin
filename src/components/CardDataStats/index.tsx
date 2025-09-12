f69d73a386a103b2fc18729a82b0ea7d11336c08
宇阳 <liuyuyang1024@yeah.net>
2025-09-12 17:48:54 +0800

    chore: 更新组件样式以优化视觉效果
    - 调整 CardDataStats 组件的样式，移除不必要的暗黑模式背景
    - 修改 DarkModeSwitcher 组件的内边距以改善布局

--- src/components/CardDataStats/index.tsx
+++ src/components/CardDataStats/index.tsx
@@ -14,7 +14,7 @@ export default ({ title, total, children }: Props) => {
       <div className="flex items-center justify-between">
         <h4 className="font-bold text-2xl my-2 text-black dark:text-white">{total}</h4>
 
-        <div className="flex h-11 w-11 items-center justify-center rounded-lg bg-[#e7f2fe] dark:bg-meta-4">{children}</div>
+        <div className="flex h-11 w-11 items-center justify-center rounded-lg bg-[#e7f2fe]">{children}</div>
       </div>
     </div>
   );
